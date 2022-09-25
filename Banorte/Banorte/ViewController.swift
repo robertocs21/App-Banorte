b0156e82a876ca8fe126bba1513b3f5589df85a7
robertocs21 <108837652+robertocs21@users.noreply.github.com>
2022-09-25 12:09:06 -0500

    Add files via upload

---
+++ Banorte/Banorte/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Banorte
+//
+//  Created by Roberto Castro on 25/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
